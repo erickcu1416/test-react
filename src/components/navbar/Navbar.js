955bef855758ce802a33e702dabf28672e0f5d30
Erick Chan <erickchan1416@hotmail.com>
2022-03-31 03:25:05 -0600

    fix navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,48 +4,50 @@ import { TOKEN_NAME_STORAGE } from '../../pages/LoginPage/constants/constants';
 import './navbar.css';
 
 export default function Navbar() {
-	const { removeItem } = useLocalStorage();
+	const { removeItem, getItem } = useLocalStorage();
 	return (
 		<div>
-			<nav className='navbar navbar-expand-lg navbar-light bg-light'>
-				<div className='container-fluid'>
-					<div className='collapse navbar-collapse' id='navbarNav'>
-						<ul className='navbar-nav'>
-							<li className='nav-item'>
-								<NavLink
-									to='/'
-									className={({ isActive }) =>
-										isActive ? ' nav-link active' : 'nav-link'
-									}
-								>
-									EmployeesPage
-								</NavLink>
-							</li>
-							<li className='nav-item'>
-								<NavLink
-									to='/upload'
-									className={({ isActive }) =>
-										isActive ? ' nav-link active' : 'nav-link'
-									}
-								>
-									UploadPage
-								</NavLink>
-							</li>
-							<li className='nav-item'>
-								<NavLink
-									onClick={() => {
-										removeItem(TOKEN_NAME_STORAGE);
-									}}
-									to='/login'
-									className='nav-link'
-								>
-									Logout
-								</NavLink>
-							</li>
-						</ul>
+			{getItem(TOKEN_NAME_STORAGE) ? (
+				<nav className='navbar navbar-expand-lg navbar-light bg-light'>
+					<div className='container-fluid'>
+						<div className='collapse navbar-collapse' id='navbarNav'>
+							<ul className='navbar-nav'>
+								<li className='nav-item'>
+									<NavLink
+										to='/'
+										className={({ isActive }) =>
+											isActive ? ' nav-link active' : 'nav-link'
+										}
+									>
+										EmployeesPage
+									</NavLink>
+								</li>
+								<li className='nav-item'>
+									<NavLink
+										to='/upload'
+										className={({ isActive }) =>
+											isActive ? ' nav-link active' : 'nav-link'
+										}
+									>
+										UploadPage
+									</NavLink>
+								</li>
+								<li className='nav-item'>
+									<NavLink
+										onClick={() => {
+											removeItem(TOKEN_NAME_STORAGE);
+										}}
+										to='/login'
+										className='nav-link'
+									>
+										Logout
+									</NavLink>
+								</li>
+							</ul>
+						</div>
 					</div>
-				</div>
-			</nav>
+				</nav>
+			) : null}
 		</div>
 	);
 }
